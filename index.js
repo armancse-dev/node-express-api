7aa5ae21f9735984ef0e4068b1f6ae8687bee66d
Arman <armanhossaincse7@gmail.com>
2023-01-05 21:17:02 +0600

    Create dynamic api, api parameter, access params

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
-
+const port = 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello from node! i am excited');
+});
+
+const users = [
+  {id: 1, name: 'Arman', email: 'armanh522@gmail.com', phone: '01991033899' },
+  {id: 2, name: 'Mim', email: 'mim@gmail.com', phone: '0199354789' },
+  {id: 3, name: 'Maruf', email: 'armanh522@gmail.com', phone: '01991033899' },
+  {id: 4, name: 'sujon', email: 'armanh522@gmail.com', phone: '01991033899' },
+  {id: 5, name: 'santo', email: 'armanh522@gmail.com', phone: '01991033899' },
+  {id: 6, name: 'johurul', email: 'armanh522@gmail.com', phone: '01991033899' },
+  {id: 7, name: 'jahangir', email: 'armanh522@gmail.com', phone: '01991033899' }
+]
+
+app.get('/users', (req, res) =>{
+  res.send(users);
+});
+
+app.get('/users/:id', (req, res) =>{
+  const id = req.params.id;
+  const user = users[id]
+  res.send(user);
 });
 
 app.listen(port, () => {
