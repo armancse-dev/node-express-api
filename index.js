d3b5f0a3c866963b5d8ff391777e9aa235c3e6b1
Arman <armanhossaincse7@gmail.com>
2023-01-05 22:45:11 +0600

    Access query parameter and return search result

--- index.js
+++ index.js
@@ -17,7 +17,14 @@ const users = [
 ]
 
 app.get('/users', (req, res) =>{
-  res.send(users);
+  const search = req.query.search;
+  if(search){
+    const searchResult = users.filter( user=> user.name.toLocaleLowerCase().includes(search) );
+    res.send(searchResult);
+  }else{
+    res.send(users);
+  }
+  
 });
 
 app.get('/users/:id', (req, res) =>{
@@ -25,6 +32,15 @@ app.get('/users/:id', (req, res) =>{
   const user = users[id]
   res.send(user);
 });
+
+app.get('/fruits/mangoes/fazli', (req, res) => {
+  res.send('Yummy Yummy tok marka fazli');
+});
+
+app.get('/fruits', (req, res) => {
+  res.send([ 'mango', 'oranges', 'banana' ]);
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
